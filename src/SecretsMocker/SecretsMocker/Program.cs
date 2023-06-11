9cf39163a80d4a56955a0b1df0ff233f4112cc76
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-06-11 08:40:40 -0400

    Update Program.cs
    Use URL rewrite to redirect to swagger page

--- src/SecretsMocker/SecretsMocker/Program.cs
+++ src/SecretsMocker/SecretsMocker/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Rewrite;
 using SecretsMocker.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,10 +15,7 @@ app.UseCors(x => x
     .AllowAnyHeader());
 
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = "";
-});
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -27,5 +25,9 @@ app.UseAuthorization();
 app.UseMiddleware<AkeylessAuthMiddleware>();
 
 app.MapControllers();
+
+var option = new RewriteOptions();
+option.AddRedirect("^$", "swagger/index.html");
+app.UseRewriter(option);
 
 app.Run();

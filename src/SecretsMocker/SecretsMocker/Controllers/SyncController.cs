3a420053abb3a110546ab8aa6c0a98032e19f98c
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-06-28 13:37:58 -0400

    Update SyncController.cs
    Changed API code to handle every scenario without tryign to mock a real secret generator. Instead the API will respond with mock secrets for a specific ID, then also be able to confirm that id has been revoked. However, at no point was this ID actually saved.

--- src/SecretsMocker/SecretsMocker/Controllers/SyncController.cs
+++ src/SecretsMocker/SecretsMocker/Controllers/SyncController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SecretsMocker.Authorization;
-using SecretsMocker.Models.AKeyless;
-using System.Text;
 using SecretsMocker.Helpers;
+using SecretsMocker.Models.AKeyless;
 
 namespace SecretsMocker.Controllers
 {
@@ -11,8 +10,6 @@ namespace SecretsMocker.Controllers
     [ApiController]
     public class SyncController : ControllerBase
     {
-        private readonly List<string> knownIds = new(){ "p-1234", "dry-run", "p-custom" };
-
         [HttpGet]
         public string Get()
         {
@@ -24,13 +21,13 @@ namespace SecretsMocker.Controllers
         [HttpPost]
         public AkeylessCreateOutput Create([FromBody] AkeylessCreateInput input)
         {
-            var generateId = MockHelpers.GenerateId();
-            knownIds.Add(generateId);
-
+            // This is a demo that doesn't really create credentials, so I am returning a fingerprint of a successful generation
+            var generatedId = MockHelpers.GenerateId();
+            
             return new AkeylessCreateOutput
             {
-                Id = generateId,
-                Response = @"{ ""cert"":""abcdefghijklmnopqrstuvwyz"", ""private_key"":""abcdefghijklmnopqrstuvwyz-now-i-know-my-abcs"" }"
+                Id = generatedId,
+                Response = GenerateSamplePayload()
             };
         }
         
@@ -39,54 +36,13 @@ namespace SecretsMocker.Controllers
         [HttpPost]
         public AkeylessRevokeOutput Revoke([FromBody] AkeylessRevokeInput input)
         {
-            // PREP //
+            // This is just an example, automatically saying all of the credentials were revoked.
+            // In a real-world application, you would take the process of revoking the ids and then returning back the results.
 
-            // In case of empty request, for example a dry-run from the Akeyless gateway.
-            if (input?.Ids == null)
-            {
-                return new AkeylessRevokeOutput
-                {
-                    Revoked = new List<string> { "p-custom" },
-                    Message = "Empty request, no credentials were revoked."
-                };
-            }
-            
-            // prepare input data
-            var matchedIds = new List<string>();
-            var missingIds = new List<string>();
-
-            input.Ids.ForEach((id =>
-            {
-                if (knownIds.Contains(id))
-                    matchedIds.Add(id);
-                else
-                    missingIds.Add(id);
-            }));
-            
-
-            // ACTIONS //
-
-            var sb = new StringBuilder(string.Empty);
-            
-            // Handle removals
-            matchedIds.ForEach(id => knownIds.Remove(id));
-
-            // Handle "unremovables"
-            if (missingIds.Count > 0)
-            {
-                sb.Append("Warning. ID(s): ");
-                sb.AppendJoin(",", missingIds);
-                sb.Append(" were not removed. User(s) not found.");
-            }
-            
-
-            // RESPOND //
-            
-            // Return result
             return new AkeylessRevokeOutput
             {
-                Revoked = matchedIds,
-                Message = sb.ToString() 
+                Revoked = input.Ids,
+                Message = "Successfully removed items."
             };
         }
 
@@ -95,12 +51,19 @@ namespace SecretsMocker.Controllers
         [HttpPost]
         public AkeylessRotateOutput Rotate([FromBody] AkeylessRotateInput input)
         {
-            var newPayload = $"UPDATED at {DateTime.UtcNow.Ticks} - {input.Payload}";
-
+            // This is just a demo. To mimic rotating a secret;
             return new AkeylessRotateOutput
             {
-                Payload = newPayload
+                Payload = GenerateSamplePayload()
             };
+        }
+
+        private string GenerateSamplePayload()
+        {
+            var payload = "{";
+            payload += $"\"updated\": \"{DateTime.UtcNow.Ticks}\", \"cert\": \"abcdefghijklmnopqrstuvwyz\", \"private_key\": \"now-i-know-my-abcs\"";
+            payload += "}";
+            return payload;
         }
     }
 }

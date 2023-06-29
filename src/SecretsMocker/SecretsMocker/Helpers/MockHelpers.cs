313c45d4519de76bac5aa11481e441efe99d68e5
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-06-29 12:58:52 -0400

    Update MockHelpers.cs
    Change id pattern

--- src/SecretsMocker/SecretsMocker/Helpers/MockHelpers.cs
+++ src/SecretsMocker/SecretsMocker/Helpers/MockHelpers.cs
@@ -2,13 +2,15 @@
 {
     public static class MockHelpers
     {
-        private const string SeedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private const string LowerSeedChars = "abcdefghijklmnopqrstuvwxyz";
+        private const string SeedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         private static readonly Random Rand = new();
         
         public static string GenerateId()
         {
-            var suffix = new string(Enumerable.Repeat(SeedChars, 6).Select(s => s[Rand.Next(s.Length)]).ToArray());
-            return $"p-{suffix}";
+            var suffixOne = new string(Enumerable.Repeat(LowerSeedChars, 5).Select(s => s[Rand.Next(s.Length)]).ToArray());
+            var suffixTwo = new string(Enumerable.Repeat(SeedChars, 27).Select(s => s[Rand.Next(s.Length)]).ToArray());
+            return $"tmp.{suffixOne}_{suffixTwo}";
         }
     }
 }

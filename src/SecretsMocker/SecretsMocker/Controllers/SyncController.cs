2a658cae4bad10728bf2ac81498c51fa13dfe13d
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-06-29 11:53:36 -0400

    Update SyncController.cs
    Changed responses to JsonResult

--- src/SecretsMocker/SecretsMocker/Controllers/SyncController.cs
+++ src/SecretsMocker/SecretsMocker/Controllers/SyncController.cs
@@ -20,55 +20,65 @@ namespace SecretsMocker.Controllers
         [AllowAnonymous]
         [Route("create")]
         [HttpPost]
-        public AkeylessCreateOutput Create([FromBody] AkeylessCreateInput input)
+        public JsonResult Create([FromBody] AkeylessCreateInput input)
         {
             // This is a demo that doesn't really create credentials, so I am returning a fingerprint of a successful generation
             var generatedId = MockHelpers.GenerateId();
-            
-            return new AkeylessCreateOutput
+
+            return new JsonResult(new AkeylessCreateOutput
             {
                 Id = generatedId,
                 Response = GenerateSamplePayload()
-            };
+            });
         }
         
         [AllowAnonymous]
         [Route("revoke")]
         [HttpPost]
-        public AkeylessRevokeOutput Revoke([FromBody] AkeylessRevokeInput input)
+        public JsonResult Revoke([FromBody] AkeylessRevokeInput input)
         {
             // This is just an example, automatically saying all of the credentials were revoked.
             // In a real-world application, you would take the process of revoking the ids and then returning back the results.
 
-            return new AkeylessRevokeOutput
+            return new JsonResult(new AkeylessRevokeOutput
             {
                 Revoked = input.Ids,
-                Message = "Successfully removed items."
-            };
+                Message = GenerateSamplePayload()
+            });
         }
 
         [AllowAnonymous]
         [Route("rotate")]
         [HttpPost]
-        public AkeylessRotateOutput Rotate([FromBody] AkeylessRotateInput input)
+        public JsonResult Rotate([FromBody] AkeylessRotateInput input)
         {
             // This is just a demo. To mimic rotating a secret;
-            return new AkeylessRotateOutput
+            return new JsonResult(new AkeylessRotateOutput
             {
                 Payload = GenerateSamplePayload()
-            };
+            });
         }
 
         private string GenerateSamplePayload()
         {
-            var certRes = new CertResponse
-            {
-                UpdatedAt = DateTime.UtcNow,
-                Cert = "now-i-know-my-abcs",
-                PrivateKey = "abcdefghijklmnopqrstuvwyz"
-            };
+            //var certRes = new CertResponse
+            //{
+            //    UpdatedAt = DateTime.UtcNow,
+            //    Cert = "now-i-know-my-abcs",
+            //    PrivateKey = "abcdefghijklmnopqrstuvwyz"
+            //};
 
-            return JsonSerializer.Serialize(certRes);
+            //return JsonSerializer.Serialize(certRes);
+
+            var response = "{'updated_at': '";
+
+            response += $"{DateTime.UtcNow.Ticks}";
+
+            response += "', 'cert': 'now-i-know-my-abcs', 'secret_key': 'abcdefghijklmnopqrstuvwyz'}";
+
+            return response;
+
+            //return "{ 'cert':'redacted', 'private_key':'redacted' }";
         }
     }
 }
